d4729e58a4716a1ef3ad9179ad2f6033afc82e0d
silicon <siliconoxide4@gmail.com>
2024-03-31 11:21:48 +0600

    updated

--- app/consumers.py
+++ app/consumers.py
@@ -1,5 +1,8 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
 from channels.exceptions import StopConsumer
+from time import sleep
+import asyncio
+
 class MySyncConsumer(SyncConsumer):
     def websocket_connect(self,event):
         print('websocket connected...')
@@ -9,21 +12,33 @@ class MySyncConsumer(SyncConsumer):
     
     def websocket_receive(self,event):
         print('websocket received...')
-        print('client msg:',event['text'])
+        for i in range(10):
+            self.send({
+            'type':'websocket.send',
+            'text':str(i)
+            })
+            sleep(1)
     
     def websocket_disconnect(self,event):
         print('websocket disconnted...')
+        raise StopConsumer()
 
 class MyAsyncConsumer(AsyncConsumer):
     async def websocket_connect(self,event):
         print('websocket connected...')
         await self.send({
-            'type':'websocket.accept'
+            'type':'websocket.accept',
         })
     
     async def websocket_receive(self,event):
         print('websocket received...')
-        print('client msg:',event['text'])
+        for i in range(10):
+            await self.send({
+            'type':'websocket.send',
+            'text':str(i)
+            })
+            await asyncio.sleep(1)
     
     async def websocket_disconnect(self,event):
         print('websocket disconnted...')
+        raise StopConsumer()
