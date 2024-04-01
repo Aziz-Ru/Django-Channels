0ea9cee991c50542e33dec8942fa59c508fcf446
silicon <siliconoxide4@gmail.com>
2024-04-01 15:09:26 +0600

    update

--- app/consumers.py
+++ app/consumers.py
@@ -1,5 +1,6 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
 from channels.exceptions import StopConsumer
+from asgiref.sync import async_to_sync
 from time import sleep
 import asyncio
 import json
@@ -8,6 +9,10 @@ class MySyncConsumer(SyncConsumer):
     # websocket.connect message is handled by websocket_connect 
     def websocket_connect(self,event):
         print('websocket connected...')
+        # print('Channel_Name..',self.channel_name)
+        # add a channel to new or existing group
+        # this asynch method == self.channel_layer.group_add('programmers',self.channel_name)
+        async_to_sync(self.channel_layer.group_add)('programmers',self.channel_name)
         self.send({
             'type':'websocket.accept'
         })
@@ -17,14 +22,22 @@ class MySyncConsumer(SyncConsumer):
         # type :websocket.receive
         # bytes: The message content if it was binary mode 
         # text: The message content if it was text mode
-        for i in range(10):
-            self.send({
+        # print('event...',event)
+        async_to_sync(self.channel_layer.group_send)('programmers',{
+            'type':'chat.message',
+            'message':event['text']
+        })
+            
+    def chat_message(self,event):
+        # print('chat..event',event)
+        self.send({
             'type':'websocket.send',
-            'text':json.dumps({'cnt':str(i)})
-            })
-            sleep(1)
+            'text':event['message']
+        })
+        
     
     def websocket_disconnect(self,event):
+        async_to_sync(self.channel_layer.group_discard)('programmers',self.channel_name)
         raise StopConsumer()
 
 class MyAsyncConsumer(AsyncConsumer):
--- app/views.py
+++ app/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+# channel layer allows you to talk between different instance of an application
+# a channel layer is the transport mechanism that allows multiple consumer instance to communicate
+# with each other
+# Redis channel layer---production
+# In memory chanel layer---for development
 def index(request):
     return render(request,'app/index.html')
+
+
+
+'''
+get_channel_layer()=this function is used to get default channnel layer from project.
+from channels.layers import get_channel_layer
+channel_layer= this attributeis used to get default channel layer from a project.
+channel_name=this attribute contains the channel name that will reach the consumer.
+send()==send('channel_name',message)
+group_send()==group_send('group_channel_name',message)
+ 
+'''
